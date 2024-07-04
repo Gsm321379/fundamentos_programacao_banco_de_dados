40d269175bbdc7dc5a2c755910e5e18713989882
GIZELLI <147009602+Gsm321379@users.noreply.github.com>
2024-07-03 21:40:47 -0300

    Laços de repetição

---
+++ condicional.py
@@ -0,0 +1,20 @@
+#print ('Olá, boa noite,')
+#idade =16
+# variavel que recebe o valor inteiro
+#if idade == 16:
+#print ('Menor de idade')
+
+#idade = int(input ('Qual é a sua idade?: '))
+#if idade <  16:
+#    print ('Você é menor de idade')
+#else: 
+#    print ('Você é maior de idade')
+
+Fetaria = int(input('Qual é a sua idade?: ')) 
+if Fetaria > 18:
+    print ('Você pode ver filme de terror')
+elif Fetaria >= 14:
+    print ('Você pode ver filme de ação')
+else        :
+    print ('Você so pode ver filme infantil')
+    
---
+++ lacosderepeticao.py
@@ -0,0 +1,14 @@
+#x=0
+#while x < 5:
+ #   nome = input ('Qual é o seu nome?')
+ #   x=x+1
+
+x=0
+animais = []
+
+while x < 5:
+    animal = input ('Qual é o nome do seu animal? ')
+    animais.append(animal)
+    x=x+1
+
+    print(animais)
