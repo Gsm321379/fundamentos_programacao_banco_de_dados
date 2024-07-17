c999b8f6ab36602c417eb67d7805a44ab5d89347
GIZELLI <147009602+Gsm321379@users.noreply.github.com>
2024-07-16 21:29:00 -0300

    Add files via upload
    função e cálculo de média.py

---
+++ calculo de media.py
@@ -0,0 +1,23 @@
+#def media_aluno(nota1,nota2,nota3):
+    #media = ((nota1+nota2+(nota3*2))/4)
+    #return media
+#lendo as notas
+#nota1 = float(input("Digite a primeira nota:"))
+#nota2 = float(input("Digite a segunda nota:"))
+#nota3 = float(input("Digite a terceira nota:"))
+#chamando a função
+#media = media_aluno(nota1,nota2,nota3)
+
+#print("A média do aluno é: ",media)
+
+def batatas(lavada,doce):
+    batata = (lavada+doce)
+    return batata
+#lendo as notas
+lavada = float(input("Digite a quantidade de batata lavada:"))
+doce = float(input("Digite a quantidade de batata doce:"))
+
+#chamando a função
+batata = batatas(lavada,doce)
+
+print("Qual é a contidade de: ",batata)
---
+++ funcao.py
@@ -0,0 +1,40 @@
+#print vem com uma mensagem de texto e ele auto executa por tela...
+#def mensagem():
+#    print("Sejam bem vindo a Proz!")
+#
+#mensagem()
+
+#def mensagem():
+#    print("Lembrete, caro cliente não esqueça de retirar suas compras no atendimento ao cliente")
+#def mensagem2(nome):
+#    print('Obrigado por comprar com a gente''+nome+')
+#mensagem2("Gizelle")
+
+def mulher():
+    print("Alta")
+    print("Baixa")
+    print("Morena")
+    print("Loira")
+m1=str(input('Você é alta ou baixa? '))
+m2=str(input('Você é morena ou loira? '))  
+
+if m1 == "Alta":
+    print("Você é uma mulher alta ")
+else: 
+    print("Você é uma mulher baixa")
+if m2 == "Morena":
+    print("Você é uma mulher morena")
+else:
+    print("Você é uma mulher loira")
+     
+
+
+
+
+    
+
+
+
+
+
+
